21b01becbcbef1156a2b5a789471e910fad3a394
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2020-10-21 15:18:14 +0700

    Remove all file exept build

---
+++ precache-manifest.abe3166f4db0977a7aeecfb91a499a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8100421aacd04e7f4b7ac499fee041",
+    "url": "/ad5dc9c24049ffbe3480180809362b3e94211cdb/index.html"
+  },
+  {
+    "revision": "9022165369db3cd2b7d2",
+    "url": "/ad5dc9c24049ffbe3480180809362b3e94211cdb/static/css/main.663eccbc.chunk.css"
+  },
+  {
+    "revision": "7aa817d147fb9a0f905f",
+    "url": "/ad5dc9c24049ffbe3480180809362b3e94211cdb/static/js/2.391559cb.chunk.js"
+  },
+  {
+    "revision": "32fb9f73882cf467122b724d78712b06",
+    "url": "/ad5dc9c24049ffbe3480180809362b3e94211cdb/static/js/2.391559cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9022165369db3cd2b7d2",
+    "url": "/ad5dc9c24049ffbe3480180809362b3e94211cdb/static/js/main.66fc1823.chunk.js"
+  },
+  {
+    "revision": "9275753c2736937e12d3",
+    "url": "/ad5dc9c24049ffbe3480180809362b3e94211cdb/static/js/runtime-main.7a2b2a1a.js"
+  }
+]);
--- src/settings.js
+++
@@ -1,2 +0,0 @@
-export const categories = ['Lunch', 'Dinner'];
-export const calBtnSize = 50;
